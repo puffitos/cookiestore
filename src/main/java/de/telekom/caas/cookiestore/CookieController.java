aa0cfeeffc8ff1fc7302ef25730aa1fff4b041c1
Bruno Bressi <bruno.bressi@telekom.de>
2023-12-06 20:22:38 +0100

    feat: get cookie by ID
    Signed-off-by: Bruno Bressi <bruno.bressi@telekom.de>

--- src/main/java/de/telekom/caas/cookiestore/CookieController.java
+++ src/main/java/de/telekom/caas/cookiestore/CookieController.java
@@ -3,6 +3,7 @@ package de.telekom.caas.cookiestore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -10,13 +11,19 @@ import java.util.List;
 @RestController
 public class CookieController {
 
-    @Autowired
-    private CookieRepository cookieRepository;
+    private final CookieRepository cookieRepository;
+
+    public CookieController(CookieRepository cookieRepository) {
+        this.cookieRepository = cookieRepository;
+    }
 
     @GetMapping("/cookies")
     public List<Cookie> getAllCookies() {
-
-        // Returning all cookies
         return cookieRepository.findAll();
+    }
+
+    @GetMapping("/cookie/{id}")
+    public Cookie getCookieById(@PathVariable Long id) {
+        return cookieRepository.findById(id).orElseThrow();
     }
 }
--- src/main/java/de/telekom/caas/cookiestore/CookieRepository.java
+++ src/main/java/de/telekom/caas/cookiestore/CookieRepository.java
@@ -4,5 +4,4 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CookieRepository extends JpaRepository<Cookie, Long> {
-  List<Cookie> findAll();
 }
